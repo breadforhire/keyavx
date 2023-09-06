8ee62a80cc765f4dea2920903191436e5f388fdd
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-09-06 16:52:20 -0400

    Update redx.cpp

--- redx.cpp
+++ redx.cpp
@@ -2,19 +2,30 @@
 #include <immintrin.h>
 
 
+#define xor128(a, b) _mm_xor_si128(a, b)
+#define and128(a, b) _mm_and_si128(a, b)
+#define rot32(x) _mm256_shuffle_epi32((x), _mm_shuffle(2, 3, 0, 1))
 
-#define GCD(a, v) \
+#define load128(p)  _mm_load_si128( (__m128i *)(p) )
+#define store128(p,r) _mm_store_si128((__m128i *)(p), r)
 
-#define AND(a, b) _mm256_and_pd(a, b)
-#define  OR(a, b) _mm256_or_si256(a, b)
+#define add128(x,y)        _mm_add_epi64(x,y)
+#define sub128(x,y)        _mm_sub_epi64(x,y)
+#define mul128(x,y)        _mm_mul_epi32(x,y)
+
+#define length 64
+
+void red0x(){
+    __m128i b, n, nx, r, t;
+    __m128i ones = _mm_set1_epi64x((uint64_t)1 >> 63);
+
+    n = and128(b, n);
+    store128(&b, n);
+    nx = sub128(b, ones);
+
+    /*b^r writing one word to heap*/
+    r = mul128(b, ones  );
 
 
-int main(){
-
-     __m256d B, N, NX, T;
-
-     B, N = AND(B, N);
-
-     return 0;
 
 }
