cb0b3f1f56bd1e36e087cf049368b9407aaeefc5
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-09-06 20:18:59 +0000

    Add files via upload

---
+++ redx.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <immintrin.h>
+
+
+
+#define GCD(a, v) \
+
+#define AND(a, b) _mm256_and_pd(a, b)
+#define  OR(a, b) _mm256_or_si256(a, b)
+
+
+int main(){
+
+     __m256d B, N, NX, T;
+
+     B, N = AND(B, N);
+
+     return 0;
+
+}
